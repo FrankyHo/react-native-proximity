49f673e7d9626ad07ac68b5fce311291e36cee73
Ruslan Deyarov <deyarov@gmail.com>
2019-09-25 16:33:26 +0400

    fix issue with invoke on background thread

--- RNProximity/RNProximity/RNProximity.m
+++ RNProximity/RNProximity/RNProximity.m
@@ -42,7 +42,10 @@
 RCT_EXPORT_MODULE();
 
 RCT_EXPORT_METHOD(proximityEnabled:(BOOL)enabled) {
-  [[UIDevice currentDevice] setProximityMonitoringEnabled:enabled];
+    dispatch_sync(dispatch_get_main_queue(), ^{
+        /* Do UI work here */
+        [[UIDevice currentDevice] setProximityMonitoringEnabled:enabled];
+    });
 }
 
 @end
